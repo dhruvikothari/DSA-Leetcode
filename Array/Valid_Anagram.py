7e1af4a5bd7aa87970aff84082e7af292aef3daa
dhruvikothari <92981344+dhruvikothari@users.noreply.github.com>
2023-07-03 15:19:49 +0530

    Create Valid_Anagram.py
    Array

---
+++ Array/Valid_Anagram.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        cnts, cntt = {}, {}
+
+        for i in range (len(s)):
+            cnts[s[i]] = 1 + cnts.get(s[i], 0)
+            cntt[t[i]] = 1 + cntt.get(t[i], 0)
+        return cnts == cntt
